16764267eb6a7dd77d463401ee1e618e1837e018
George Maddocks <gwpmad@Georges-MacBook-Pro.local>
2015-11-06 15:20:52 +0000

    added password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
+  include BCrypt
 
   has n, :links, :through => Resource
 
   property :id, Serial
   property :username, String
   property :email, String
-  property :password, String
+  property :password_hash, Text
 
+  def password=(password)#overrides the <property name>= method but only for properties named 'password.' So rather than saving it under a password column in the db, the pw is hashed and transferred to a password_hash column
+    self.password_hash = BCrypt::Password.create(password)
 end
